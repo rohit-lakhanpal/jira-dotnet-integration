56a44ae61c75e6e1642ade8b6a47e12b151cda6d
Rohit Lakhanpal <mail@rohit-lakhanpal.info>
2016-04-11 17:07:22 +1000

    Initial commit to add Panviva.JiraDotNetIntegration.Library and Panviva.JiraDotNetIntegration.Library.Tests projects to the Panviva.JiraDotNetIntegration solution.

---
+++ Panviva.JiraDotNetIntegration/src/Panviva.JiraDotNetIntegration.Library.Tests/TestSampleClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Panviva.JiraDotNetIntegration.Library.Tests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class TestSampleClass
+    {
+        public TestSampleClass()
+        {
+        }
+
+        [Fact]
+        public void Test_DoWork()
+        {
+            Assert.Equal("work", new SampleClass().DoWork());
+        }
+    }
+}
